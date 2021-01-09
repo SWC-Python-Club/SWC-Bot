88fcb7549891ff38efcb43d10a2bf02e3ad944dc
AndreiSva <andreisva2023@gmail.com>
2021-01-09 11:13:41 -0800

    added command error and moved around some things

--- src/git.py
+++ src/git.py
@@ -6,24 +6,19 @@ import json
 
 import helpers
 
-@commands.command()
-async def version(ctx):
-    '''fetch the version of this bot'''
-    await ctx.send(embed=discord.Embed(
-            title="SWC Python Bot",
-            description="1.0"))
-
-@commands.command()
-async def git(ctx, name=None):
-    '''fetch information from a git repository on our official github organization (SWC-Python-Club)''' 
-    if name is None:
-        await ctx.send(embed=helpers.ArgumentError(1).as_embed())
-        return
-    repositories = requests.get("https://api.github.com/orgs/SWC-Python-Club/repos")
-    print(repositories.json())
-    if name not in repositories.json():
-        await ctx.send(embed=discord.Embed(
+class fetches(commands.Cog):
+    '''a set of tools to display about a number of things'''
+    @commands.command()
+    async def git(ctx, name=None):
+        '''fetch information from a git repository on our official github organization (SWC-Python-Club)''' 
+        if name is None:
+            await ctx.send(embed=helpers.ArgumentError(1).as_embed())
+            return
+        repositories = requests.get("https://api.github.com/orgs/SWC-Python-Club/repos")
+        for repo in repositories.json():
+            if name in repo["name"]:
+                pass
+                return
+        await ctx.send(embed=discord.Embed( 
             title="Error",
             description="unable to fetch repository"))
-    else:
-        pass
--- src/main.py
+++ src/main.py
@@ -14,10 +14,34 @@ import helpers
 prefix = '.'
 bot = commands.Bot(command_prefix=prefix)
 
+@bot.command()
+async def version(ctx):
+    '''fetch the version of this bot'''
+    await ctx.send(embed=discord.Embed(
+            title="SWC Python Bot",
+            description="1.0"))
+
+@bot.event
+async def on_message(message):
+    if message.author != bot.user:
+        if "vim" in message.content.lower():
+            await message.channel.send("The best editor!!!")
+    
+    await bot.process_commands(message)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        print("Unrecognized command")
+        await ctx.send(embed=discord.Embed(
+            title="Error",
+            description="Unrecognized command!!!",
+            color=0xff0000
+            ))
+
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name=prefix + "help"))
 
-bot.add_command(git.version)
-bot.add_command(git.git)
+bot.add_cog(git.fetches())
 bot.run(os.environ["token"])
